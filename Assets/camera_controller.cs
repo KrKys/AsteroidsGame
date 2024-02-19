f91b8d82e2428b76c35ec0bf579ec838c4f8039f
Oskar Zaza <oskar.zaza@teb.edu.pl>
2024-02-19 09:32:08 +0100

    camera controller

--- Assets/camera_controller.cs
+++ Assets/camera_controller.cs
@@ -21,7 +21,7 @@ public class camera_controller : MonoBehaviour
         Vector3 targetPosition = player.position + Vector3.up * cameraHeight;
         //p³ynnie przesuñ kamere w kierunku gracza
         //funkcja Vector3.Lerp
-        //p³ynnie przechodzi z pozycji pierwszeo argumentu do drugiego w czasie trzeciego
+        //p³ynnie przechodzi z pozycji pierwszeo argumentu do drugiego w czasie trzeciegoo
         transform.position =Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
     }
 }
